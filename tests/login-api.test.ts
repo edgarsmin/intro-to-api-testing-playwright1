0b9edde8f1bf117b7b02b0e32274b48b8ca79b5f
edgars <edgars.mintals@gmail.com>
2024-12-24 05:33:00 +0200

    Upgrade positive test and adding new negative test

--- tests/login-api.test.ts
+++ tests/login-api.test.ts
@@ -5,37 +5,35 @@ const serviceURL = 'https://backend.tallinn-learning.ee/'
 const loginPath = 'login/student'
 
 test.describe('Tallinn delivery API tests', () => {
-  test('login with VALID data', async ({request}) => {
+  test('login with VALID data', async ({ request }) => {
     const requestBody = LoginDto.createLoginWithCorrectData()
 
     const response = await request.post(`${serviceURL}${loginPath}`, {
-      data: requestBody
+      data: requestBody,
     })
     expect(response.status()).toBe(StatusCodes.OK)
     const responseBody = await response.text()
     console.log('Response body:', responseBody)
-    expect(responseBody).toMatch(
-      /^eyJhb[A-Za-z0-9-_]+\.[A-Za-z0-9-_]+\.[A-Za-z0-9-_]+$/
-    )
+    expect(responseBody).toMatch(/^eyJhb[A-Za-z0-9-_]+\.[A-Za-z0-9-_]+\.[A-Za-z0-9-_]+$/)
   })
-  test('login with incorrect data', async ({request}) => {
+  test('login with incorrect data', async ({ request }) => {
     const requestBody = LoginDto.createLoginWithIncorrectData()
 
     const response = await request.post(`${serviceURL}${loginPath}`, {
-      data: requestBody
+      data: requestBody,
     })
     expect(response.status()).toBe(401)
   })
   test('login with incorrect HTTP method', async ({ request }) => {
-    const requestBody = LoginDto.createLoginWithCorrectData();
+    const requestBody = LoginDto.createLoginWithCorrectData()
 
     const response = await request.get(`${serviceURL}${loginPath}`, {
       data: requestBody,
-    });
+    })
 
-    expect(response.status()).toBe(StatusCodes.METHOD_NOT_ALLOWED);
+    expect(response.status()).toBe(StatusCodes.METHOD_NOT_ALLOWED)
 
-    const responseBody = await response.text();
-    console.log('Response body:', responseBody);
-  });
+    const responseBody = await response.text()
+    console.log('Response body:', responseBody)
+  })
 })
